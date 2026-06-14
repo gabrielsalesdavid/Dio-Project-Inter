28e49898683c39def53ee02ebdeda8745a339f7a
Gabriel Sales David <gabriel.sales.david@gmail.com>
2026-06-14 16:09:31 -0300

    Criação de classes e pasta de sobrecarga. Class Main e Quadrilatero.

---
+++ src/linguagemjava/sobrecarga/Main.java
@@ -0,0 +1,12 @@
+package sobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Exercício sobrecarga de métodos");
+        Quadrilatero.areaQuadratia(5);
+        Quadrilatero.areaRetangular(5, 10);
+        Quadrilatero.areaTrapezoidal(7, 5, 3);
+        Quadrilatero.areaLosango(4, 6);
+    }
+}
---
+++ src/linguagemjava/sobrecarga/Quadrilatero.java
@@ -0,0 +1,24 @@
+package sobrecarga;
+
+public class Quadrilatero {
+    
+    public static void areaQuadratia(double lado) {
+
+        System.out.println("Área do quadrado: " + lado * lado);
+    }
+
+    public static void areaRetangular(double lado1, double lado2) {
+
+        System.out.println("Área do retângulo: " + lado1 * lado2);
+    }
+
+    public static void areaTrapezoidal(double baseMaior, double baseMenor, double altura) {
+
+        System.out.println("Área do trapézio: " + ((baseMaior + baseMenor) * altura) / 2);
+    }
+
+    public static void areaLosango(double diagonal1, double diagonal2) {
+
+        System.out.println("Área do losango: " + (diagonal1 * diagonal2) / 2);
+    }
+}
