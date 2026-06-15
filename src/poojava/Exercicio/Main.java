967ec114d45603d82715308591dbcad61e391c52
Gabriel Sales David <gabriel.sales.david@gmail.com>
2026-06-15 08:29:24 -0300

    Instanciando a classe carro na classe principal...

--- src/poojava/Exercicio/Main.java
+++ src/poojava/Exercicio/Main.java
@@ -5,5 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
         Carro carro1 = new Carro();
+
+        carro1.setCor("Preto");
+        carro1.setModelo("Celta");
+        carro1.setCapacidadeTanque(50);
+
+        System.out.println("Cor: " + carro1.getCor());
+        System.out.println("Modelo: " + carro1.getModelo());
+        System.out.println("Capacidade do tanque: " + carro1.getCapacidadeTanque());
+        System.out.println("Valor total para encher o tanque: " + carro1.totalValor(5.50));
+
+        Carro carro2 = new Carro("Branco", "Onix", 60);
+        System.out.println("Cor: " + carro2.getCor());
+        System.out.println("Modelo: " + carro2.getModelo());
+        System.out.println("Capacidade do tanque: " + carro2.getCapacidadeTanque());
+        System.out.println("Valor total para encher o tanque: " + carro2.totalValor(5.50));
     }
 }
