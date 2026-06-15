adfef4bd0faa1ac95fabe8afa0ef2d4480721e70
Gabriel Sales David <gabriel.sales.david@gmail.com>
2026-06-15 11:51:58 -0300

    Criação de interface e class para calculo.

---
+++ src/poojava/Exercicio/interface/IOperacaoMatematica.java
@@ -0,0 +1,8 @@
+package poojava.Exercicio.interface;
+
+public interface IOperacaoMatematica {
+    double somar(double a, double b);
+    double subtrair(double a, double b);
+    double multiplicar(double a, double b);
+    double dividir(double a, double b);
+}
