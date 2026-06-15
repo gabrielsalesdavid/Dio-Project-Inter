b794c3c865c6cbfa6532ebbcb9ff57dbcb4c18de
Gabriel Sales David <gabriel.sales.david@gmail.com>
2026-06-15 07:58:00 -0300

    Parametrizando o lombok para amenizar a questão verbose do Java!
    Setando como comentario na classe global o construtor e os paramentros get e set.

--- src/poojava/Exercicio/Carro.java
+++ src/poojava/Exercicio/Carro.java
@@ -1,4 +1,15 @@
 package poojava.Exercicio;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter
+import lombok.Setter;
+
+@AllConstructors
+@Getter
+@Setter
 public class Carro {    
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
 }
