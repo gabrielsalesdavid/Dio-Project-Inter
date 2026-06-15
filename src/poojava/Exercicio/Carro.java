5751dc35e6c4f40a4ad9c406e0360f0c5ea54754
Gabriel Sales David <gabriel.sales.david@gmail.com>
2026-06-15 08:13:32 -0300

    Parametrizando os contrutores com e sem argumentos para a classe e, method do tipo double totalValor.

--- src/poojava/Exercicio/Carro.java
+++ src/poojava/Exercicio/Carro.java
@@ -1,10 +1,12 @@
 package poojava.Exercicio;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Getter
 import lombok.Setter;
 
 @AllConstructors
+@NoArgsConstructors
 @Getter
 @Setter
 public class Carro {    
@@ -12,4 +14,9 @@ public class Carro {
     String cor;
     String modelo;
     int capacidadeTanque;
+
+    public double totalValor(double valorCombustivel) {
+        
+        return capacidadeTanque * valorCombustivel;
+    }
 }
